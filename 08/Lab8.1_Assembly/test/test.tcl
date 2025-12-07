c02f8ea3ba1b68ddfe7892926f0e4adb6f104495
Anıl Burak Gökce <anilburakgokce@gmail.com>
2025-12-07 23:45:25 +0100

    Lab8.1 RVfpga-Trace task completed

---
+++ 08/Lab8.1_Assembly/test/test.tcl
@@ -0,0 +1,9 @@
+gtkwave::addSignalsFromList rvfpgasim.clk
+
+gtkwave::addSignalsFromList rvfpgasim.swervolf.swerv_eh1.swerv.dec.arf.gpr_banks(0).gpr(9).gprff.dout
+
+gtkwave::addSignalsFromList rvfpgasim.swervolf.timer_ptc.rptc_cntr
+gtkwave::addSignalsFromList rvfpgasim.swervolf.timer_ptc.rptc_ctrl
+gtkwave::addSignalsFromList rvfpgasim.swervolf.timer_ptc.rptc_hrc
+gtkwave::addSignalsFromList rvfpgasim.swervolf.timer_ptc.rptc_lrc
+
