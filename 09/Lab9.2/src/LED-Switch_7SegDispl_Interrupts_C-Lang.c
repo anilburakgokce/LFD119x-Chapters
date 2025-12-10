5f26183523b2bf98b64933c30fee714430634613
Anıl Burak Gökce <anilburakgokce@gmail.com>
2025-12-10 11:10:04 +0100

    Lab9.2 completed

---
+++ 09/Lab9.2/src/LED-Switch_7SegDispl_Interrupts_C-Lang.c
@@ -0,0 +1,177 @@
+#include "psp_api.h"
+#include "bsp_external_interrupts.h"
+#include "psp_ext_interrupts_eh1.h"
+#include "bsp_timer.h"
+#include "bsp_printf.h"
+
+#define GPIO_SWs        0x80001400
+#define GPIO_LEDs       0x80001404
+#define GPIO_INOUT      0x80001408
+#define RGPIO_INTE      0x8000140C
+#define RGPIO_PTRIG     0x80001410
+#define RGPIO_CTRL      0x80001418
+#define RGPIO_INTS      0x8000141C
+
+#define GPIO2_PBs       0x80001800
+#define GPIO2_INOUT     0x80001808
+#define RGPIO2_INTE     0x8000180C
+#define RGPIO2_PTRIG    0x80001810
+#define RGPIO2_CTRL     0x80001818
+#define RGPIO2_INTS     0x8000181C
+
+#define RPTC_CNTR       0x80001200
+#define RPTC_HRC        0x80001204
+#define RPTC_LRC        0x80001208
+#define RPTC_CTRL       0x8000120c
+
+#define Select_INT      0x80001018
+
+#define fastStepPeriod 0x2000
+#define slowStepPeriod 0x20000
+
+extern D_PSP_DATA_SECTION D_PSP_ALIGNED(1024) pspInterruptHandler_t G_Ext_Interrupt_Handlers[8];
+
+int LEDs_count = 0;
+int count_period = slowStepPeriod;
+
+void DefaultInitialization(void)
+{
+  u32_t uiSourceId;
+
+  /* Register interrupt vector */
+  pspInterruptsSetVectorTableAddress(&M_PSP_VECT_TABLE);
+
+  /* Set external-interrupts vector-table address in MEIVT CSR */
+  pspExternalInterruptSetVectorTableAddress(G_Ext_Interrupt_Handlers);
+
+  /* Put the Generation-Register in its initial state (no external interrupts are generated) */
+  bspInitializeGenerationRegister(D_PSP_EXT_INT_ACTIVE_HIGH);
+
+  for (uiSourceId = D_BSP_FIRST_IRQ_NUM; uiSourceId <= D_BSP_LAST_IRQ_NUM; uiSourceId++)
+  {
+    /* Make sure the external-interrupt triggers are cleared */
+    bspClearExtInterrupt(uiSourceId);
+  }
+
+  /* Set Standard priority order */
+  pspExtInterruptSetPriorityOrder(D_PSP_EXT_INT_STANDARD_PRIORITY);
+
+  /* Set interrupts threshold to minimal (== all interrupts should be served) */
+  pspExtInterruptsSetThreshold(M_PSP_EXT_INT_THRESHOLD_UNMASK_ALL_VALUE);
+
+  /* Set the nesting priority threshold to minimal (== all interrupts should be served) */
+  pspExtInterruptsSetNestingPriorityThreshold(M_PSP_EXT_INT_THRESHOLD_UNMASK_ALL_VALUE);
+}
+
+void ExternalIntLine_Initialization(u32_t uiSourceId, u32_t priority, pspInterruptHandler_t pTestIsr)
+{
+  /* Set Gateway Interrupt type (Level) */
+  pspExtInterruptSetType(uiSourceId, D_PSP_EXT_INT_LEVEL_TRIG_TYPE);
+
+  /* Set gateway Polarity (Active high) */
+  pspExtInterruptSetPolarity(uiSourceId, D_PSP_EXT_INT_ACTIVE_HIGH);
+
+  /* Clear the gateway */
+  pspExtInterruptClearPendingInt(uiSourceId);
+
+  /* Set IRQ4 priority */
+  pspExtInterruptSetPriority(uiSourceId, priority);
+    
+  /* Enable IRQ4 interrupts in the PIC */
+  pspExternalInterruptEnableNumber(uiSourceId);
+
+  /* Register ISR */
+  G_Ext_Interrupt_Handlers[uiSourceId] = pTestIsr;
+}
+
+void GPIO_Initialization(void)
+{
+  /* Configure LEDs and Switches */
+  M_PSP_WRITE_REGISTER_32(GPIO_INOUT, 0xFFFF);        /* GPIO_INOUT */
+  M_PSP_WRITE_REGISTER_32(GPIO_LEDs, 0x0);            /* GPIO_LEDs */
+
+  /* Configure Switches interrupts */
+  M_PSP_WRITE_REGISTER_32(RGPIO_INTE, 0x10000);       /* RGPIO_INTE */
+  M_PSP_WRITE_REGISTER_32(RGPIO_PTRIG, 0x10000);      /* RGPIO_PTRIG */
+  M_PSP_WRITE_REGISTER_32(RGPIO_INTS, 0x0);           /* RGPIO_INTS */
+  M_PSP_WRITE_REGISTER_32(RGPIO_CTRL, 0x1);           /* RGPIO_CTRL */
+
+
+  /* Configure Pushbuttons */
+  M_PSP_WRITE_REGISTER_32(GPIO2_INOUT, 0);             /* GPIO_INOUT */
+
+  /* Configure Pushbuttons interrupts */
+  M_PSP_WRITE_REGISTER_32(RGPIO2_INTE, 0x2);           /* RGPIO_INTE */
+  M_PSP_WRITE_REGISTER_32(RGPIO2_PTRIG, 0x0);          /* RGPIO_PTRIG */
+  M_PSP_WRITE_REGISTER_32(RGPIO2_INTS, 0x0);           /* RGPIO_INTS */
+  M_PSP_WRITE_REGISTER_32(RGPIO2_CTRL, 0x1);           /* RGPIO_CTRL */
+}
+
+void PTC_Initialization(void){
+
+  M_PSP_WRITE_REGISTER_32(RPTC_CTRL,0xC0);
+  M_PSP_WRITE_REGISTER_32(RPTC_LRC,count_period);
+  M_PSP_WRITE_REGISTER_32(RPTC_CTRL,0x21);
+}
+
+void PTC_ISR(void){
+  // write LEDs
+  M_PSP_WRITE_REGISTER_32(GPIO_LEDs, LEDs_count++);
+
+  /* Restart PTC */
+  M_PSP_WRITE_REGISTER_32(RPTC_CNTR,0x0);
+  M_PSP_WRITE_REGISTER_32(RPTC_CTRL,0x40);
+  M_PSP_WRITE_REGISTER_32(RPTC_CTRL,0x21);
+
+  /* Stop the generation of the specific external interrupt */
+  bspClearExtInterrupt(3);
+}
+
+void GPIO_ISR(void)
+{
+  /* check if the switch generated an interrupt */
+  if(M_PSP_READ_REGISTER_32(RGPIO_INTS) & 0x10000){
+    /* Reset the LEDs to 0 and restart the timer */
+    LEDs_count = 0;
+    PTC_ISR();
+
+    /* Clear Switch interrupt */
+    M_PSP_WRITE_REGISTER_32(RGPIO_INTS, 0x0);
+  }
+  
+  /* check if the center pushbutton generated an interrupt */
+  if(M_PSP_READ_REGISTER_32(RGPIO2_INTS) & 0x2){
+    // alternate the speed
+    count_period ^= slowStepPeriod ^ fastStepPeriod;
+
+    // reinitialize the counter
+    PTC_Initialization();
+
+    /* Clear Pushbuttons interrupt */
+    M_PSP_WRITE_REGISTER_32(RGPIO2_INTS, 0x0);
+  }
+
+  /* Stop the generation of the specific external interrupt */
+  bspClearExtInterrupt(4);
+}
+
+int main(void)
+{
+  /* INITIALIZE THE INTERRUPT SYSTEM */
+  DefaultInitialization();                            /* Default initialization */
+  pspExtInterruptsSetThreshold(5);                    /* Set interrupts threshold to 5 */
+
+  /* INITIALIZE INTERRUPT LINE IRQ4 AND IRQ3*/
+  ExternalIntLine_Initialization(4, 7, GPIO_ISR);     /* Initialize line IRQ4 with a priority of 6. Set GPIO_ISR as the Interrupt Service Routine */
+  ExternalIntLine_Initialization(3, 8, PTC_ISR);      /* Initialize line IRQ3 with a priority of 6. Set PTC_ISR as the Interrupt Service Routine */
+  M_PSP_WRITE_REGISTER_32(Select_INT, 0x3);           /* Connect the GPIO and PTC interrupts to IRQ4 and IRQ3 interrupt lines, respectively */
+
+  /* INITIALIZE THE PERIPHERALS */
+  GPIO_Initialization();                              /* Initialize the GPIO */
+  PTC_Initialization();                               /* Initialize the PTC */
+
+  /* ENABLE INTERRUPTS */
+  pspInterruptsEnable();                              /* Enable all interrupts in mstatus CSR */
+  M_PSP_SET_CSR(D_PSP_MIE_NUM, D_PSP_MIE_MEIE_MASK);  /* Enable external interrupts in mie CSR */
+}
+
